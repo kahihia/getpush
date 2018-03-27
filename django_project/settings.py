7756af00b69629207b0f3e904e25c70101621c65
Samuel nganga <samien09@gmail.com>
2018-03-27 02:51:37 +0000

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -164,9 +164,9 @@ if IS_LOCAL:
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 SITE_ID = 1
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
