81a268ee179584a4f346c8daabaa99554a1b7720
Samuel nganga <samien09@gmail.com>
2018-03-27 00:35:05 +0000

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -97,33 +97,20 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-if IS_LOCAL:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-    #         'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'),  # Or path to database file if using sqlite3.
-    #         'USER': '',                      # Not used with sqlite3.
-    #         'PASSWORD': '',                  # Not used with sqlite3.
-    #         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-    #         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    #     }
-    # }
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'pushmonkey',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-    }
-else:
-    DATABASES = {
-	'default': dj_database_url.config(
-	     default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-	)
-    }
+DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+             'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'),  # Or path to database file if using sqlite3.
+             'USER': '',                      # Not used with sqlite3.
+             'PASSWORD': '',                  # Not used with sqlite3.
+             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+         }
+     }
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
