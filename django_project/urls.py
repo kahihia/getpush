efe3603a55a09634735697f7ab4bf0cc47cdf3c3
Samuel nganga <samien09@gmail.com>
2018-03-27 02:37:17 +0000

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -143,6 +143,6 @@ urlpatterns = patterns('',
                        url(r'^banner-mobile/', TemplateView.as_view(template_name='agent.html'), name='banner-mobile'),
                        url(r'^banner-smobile/', TemplateView.as_view(template_name='agent-schdule.html'), name='banner-smobile'),
                        url(r'^admin/', include(admin.site.urls)),
-                       ) += patterns('',
-  (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
+                       url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
+                       ) 
 
